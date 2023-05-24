e44b74d7d1f8cbf0bd23e28179fa6b101e575b98
RodriguesPattricia <106637028+RodriguesPattricia@users.noreply.github.com>
2023-05-24 01:04:16 -0300

    Questoes Praticas Prova

--- Exercicio7/src/Main.java
+++ Exercicio7/src/Main.java
@@ -1,5 +1,25 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(1000);
+        Random random = new Random();
+
+        for (int i = 0; i < 1000; i++) {
+            vetor.adiciona(random.nextInt(100));
+        }
+
+        long inicio = System.nanoTime();
+        System.out.println("Maior valor: " + vetor.encontraMaiorRecursivo(vetor, 0, 999));
+        long fim = System.nanoTime(); 
+
+        System.out.println("Tempo de execução do método recursivo: " + (fim - inicio) + "ns");
+
+        long inicio2 = System.nanoTime();
+        System.out.println("Maior valor: "+vetor.encontraMaiorIterativo(vetor));
+        long fim2 = System.nanoTime();
+
+        System.out.println("Tempo de execução do método iterativo: " + (fim2 - inicio2) + "ns");
     }
 }
--- Exercicio8/src/Main.java
+++ Exercicio8/src/Main.java
@@ -1,5 +1,26 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(1000);
+        Random random = new Random();
+
+        for (int i = 0; i < 1000; i++) {
+            vetor.adiciona(random.nextInt(100));
+        }
+        vetor.ordenar(vetor);
+
+        int valor = vetor.get(random.nextInt(1000));
+
+        long inicio = System.nanoTime();
+        System.out.println("O valor é: "+vetor.pesquisaBinariaIterativa(vetor, valor));
+        long fim = System.nanoTime(); 
+        System.out.println("Tempo de execução do método iterativo: " + (fim - inicio) + "ns");
+
+        long inicio2 = System.nanoTime();
+        System.out.println("O valor é: "+vetor.pesquisaBinariaRecursiva(vetor, 0, 999, valor));
+        long fim2 = System.nanoTime();
+        System.out.println("Tempo de execução do método recursivo: " + (fim2 - inicio2) + "ns");
     }
 }
