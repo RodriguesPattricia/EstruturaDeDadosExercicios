9ec0142b6af17b17bdb4a3e86156953fefc1ec4e
RodriguesPattricia <106637028+RodriguesPattricia@users.noreply.github.com>
2023-06-20 16:07:31 -0300

    exercicio14

--- Exercicio14/src/Main.java
+++ Exercicio14/src/Main.java
@@ -1,5 +1,24 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Main {
+    public static void main(String[] args) {
+
+        BuscaBinaria minhaArvore = new BuscaBinaria(); 
+
+        minhaArvore.recursiveAdd(20);
+        minhaArvore.recursiveAdd(10);
+        minhaArvore.recursiveAdd(30);
+        minhaArvore.recursiveAdd(5);
+        minhaArvore.recursiveAdd(15);
+        minhaArvore.recursiveAdd(25);
+        minhaArvore.recursiveAdd(35);
+        
+
+        minhaArvore.buscar(minhaArvore.getRaiz(), 20); 
+
+        System.out.println("Imprimindo a minhaArvore: "); 
+        minhaArvore.printTodaArvore(minhaArvore.getRaiz()); 
+
+        System.out.println("A raiz da minhaArvore é: " + minhaArvore.getRaiz().valor); 
+
     }
+
 }
